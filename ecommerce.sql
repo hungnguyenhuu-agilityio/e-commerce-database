e42219fbccdc789c59ea4a9c65bed0a617649a0b
Hung Nguyen Huu <hung.nguyenhuu@asnet.com.vn>
2025-02-13 17:26:04 +0700

    feat(database): update

--- ecommerce.sql
+++ ecommerce.sql
@@ -4,15 +4,17 @@
 
 DROP TABLE IF EXISTS [Product];
 DROP TABLE IF EXISTS [Category];
-DROP TABLE IF EXISTS [Order];
-DROP TABLE IF EXISTS [OrderItem];
-DROP TABLE IF EXISTS [ShippingInfo];
-DROP TABLE IF EXISTS [PaymentMethod];
-DROP TABLE IF EXISTS [ShippingCarrier];
 
 /*****
     CREATE TABLES
 *****/
+CREATE TABLE [Category] (
+    [CategoryId] INTEGER NOT NULL,
+    [Name] VARCHAR(255) NOT NULL,
+    [ReturnDays] INTEGER NOT NULL,
+    [Condition] VARCHAR (255),
+    CONSTRAINT [PK_Category] PRIMARY KEY ([CategoryId])
+);
 
 CREATE TABLE [Product] (
     [ProductId] INTEGER NOT NULL,
@@ -26,47 +28,6 @@ CREATE TABLE [Product] (
         ON UPDATE NO ACTION
 );
 
-CREATE TABLE [Category] (
-    [CategoryId] INTEGER NOT NULL,
-    [Name] VARCHAR(255) NOT NULL,
-    [ReturnDays] INTEGER NOT NULL,
-    [Condition] VARCHAR (255),
-    CONSTRAINT [PK_Category] PRIMARY KEY ([CategoryId])
-);
-
-CREATE TABLE [Order] (
-    [OrderId] INTEGER NOT NULL,
-    CONSTRAINT [PK_Order] PRIMARY KEY ([OrderId])
-);
-
-CREATE TABLE [OrderItem] (
-    [OrderItemId] INTEGER NOT NULL,
-    [OrderId] INTEGER NOT NULL,
-    [ProductId] INTEGER NOT NULL,
-    [Quantity] INTEGER NOT NULL,
-    CONSTRAINT [PK_OrderItem] PRIMARY KEY ([OrderItemId])
-    FOREIGN KEY ([OrderId]) REFERENCES [Order]([OrderId])
-    FOREIGN KEY ([ProductId]) REFERENCES [Product]([ProductId])
-);
-
-CREATE TABLE [PaymentMethod] (
-    [PaymentMethodId] INTEGER NOT NULL,
-    [Method] VARCHAR(255) NOT NULL,
-    CONSTRAINT [PK_PaymentMethod] PRIMARY KEY ([PaymentMethodId])
-);
-
-CREATE TABLE [ShippingInfo] (
-    [ShippingInfoId] INTEGER NOT NULL,
-    [Option] VARCHAR(255) NOT NULL,
-    [DeliveryDate] VARCHAR(255) NOT NULL,
-    CONSTRAINT [PK_ShippingInfo] PRIMARY KEY ([ShippingInfoId])
-);
-
-CREATE TABLE [ShippingCarrier] (
-    [ShippingCarrierId] INTEGER NOT NULL,
-    [Carrier] VARCHAR(255) NOT NULL,
-    CONSTRAINT [PK_ShippingCarrier] PRIMARY KEY ([ShippingCarrierId])
-);
 
 /*****
     INSERT DATA
@@ -89,22 +50,3 @@ VALUES
     ('Sony WH-1000XM4', 'The Sony WH-1000XM4 is a wireless headphones with a noise-cancellation feature, a 40-hour battery life, and a long-lasting battery.', 299, 3),
     ('Nike Air Max 270', 'The Nike Air Max 270 is a comfortable sneaker with a high-top design and a responsive midsole.', 89, 4),
     ('Levi 501', 'The Levi 501 is a classic dress shirt with a high-quality cotton blend and a classic cut.', 19, 5);
-
-INSERT INTO [PaymentMethod] ([Method])
-VALUES
-    ('Credit/Debit Card'),
-    ('PayPal'),
-    ('Apple Pay'),
-    ('Google Pay');
-
-INSERT INTO [ShippingCarrier] ([Carrier])
-VALUES
-    ('FedEx'),
-    ('UPS'),
-    ('USPS');
-
-INSERT INTO [ShippingInfo] ([Option], [DeliveryDate])
-VALUES
-    ('Standard Shipping', '5-7 business days'),
-    ('Expedited Shipping', '2-3 business days'),
-    ('Next-Day Shipping', '1 business day');
